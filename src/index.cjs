bff02f2174c44eb33e106f06a8e34cebbfda45dc
Figby222 <figby222@outlook.com>
2024-08-31 15:27:30 -0700

    Add get response on /*/messages path

--- src/index.cjs
+++ src/index.cjs
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.status(200).send("Hi, this is the root page");
 })
 
+app.get("/*/messages", (req, res) => {
+    res.status(200).send("Hi, these are some messages: <ul><li>Hi</li><li>Hi 2</li></ul>");
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
