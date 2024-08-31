e3c2a4c4b1f3e939a3ce541321745eead07ba84d
Figby222 <figby222@outlook.com>
2024-08-31 15:32:23 -0700

    End HTTP response at the end of callback functions

--- src/index.cjs
+++ src/index.cjs
@@ -4,10 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.status(200).send("Hi, this is the root page");
+    res.end();
 })
 
 app.get("/:username/messages", (req, res) => {
     res.status(200).send(`Hi, these are some messages: <ul><li>Hi</li><li>Hi 2</li></ul><p>Your username is ${req.params.username}</p>`);
+    res.end();
 })
 
 app.listen(process.env.PORT, () => {
