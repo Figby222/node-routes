8a17f7f94d4f64aaadf79d862ca3513b3584892f
Figby222 <figby222@outlook.com>
2024-08-31 15:25:11 -0700

    Add get response on root path

--- src/index.cjs
+++ src/index.cjs
@@ -2,6 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+app.get("/", (req, res) => {
+    res.status(200).send("Hi, this is the root page");
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
