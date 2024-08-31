ed668ee93bc3d27517a82130a08a92c616e2e3a2
Figby222 <figby222@outlook.com>
2024-08-31 15:41:29 -0700

    Use req query props to print cookies query
    Use req query props to print cookies query from
    URL

--- src/index.cjs
+++ src/index.cjs
@@ -8,7 +8,8 @@ app.get("/", (req, res) => {
 })
 
 app.get("/:username/messages", (req, res) => {
-    res.status(200).send(`Hi, these are some messages: <ul><li>Hi</li><li>Hi 2</li></ul><p>Your username is ${req.params.username}</p>`);
+    res.status(200).send(`Hi, these are some messages: <ul><li>Hi</li><li>Hi 2</li></ul><p>Your username is ${req.params.username}</p>
+        <p>Your cookies are ${req.query.cookies === "true" ? true : req.query.cookies === "false" ? false : "invalid"}</p>`);
     res.end();
 })
 
